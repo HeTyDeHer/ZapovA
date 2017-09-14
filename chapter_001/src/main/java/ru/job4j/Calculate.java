e9df023fc900d26c4d67f467e509c7c5fc991dd6
HeTyDeHer <dolboeb88@gmail.com>
2017-09-14 17:22:16 +0300

    task 4087
    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j;
 
+/**
+*решение задачи 1 /001
+*@author ZapovA
+*@since 14.9.17
+*
+*/
+
 public class Calculate {
 	public static void main (String [] args){
 		System.out.print("HelloWorld!");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author ZapovA
+* @version $Id$
+* @since 0.1 
+*/
