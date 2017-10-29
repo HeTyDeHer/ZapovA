14f08b0248e0a3e2271204cb2af1cb8e0d738b3e
HeTyDeHer <dolboeb88@gmail.com>
2017-10-29 19:38:41 +0300

    5.3.2. Создать контейнер на базе связанного списка [ #159] fix теста.

--- chapter_005/src/test/java/list/SimpleLinkedListTest.java
+++ chapter_005/src/test/java/list/SimpleLinkedListTest.java
@@ -14,45 +14,53 @@ import static org.junit.Assert.assertThat;
  */
 public class SimpleLinkedListTest {
     /**
+     * Тест метода remove() без избыточного поведения.
+     * Добавляем 1 элемент.
+     * Убеждаемся, что remove его удаляет.
+     */
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void deleteTest() {
+        SimpleLinkedList<String> linklist = new SimpleLinkedList<>();
+        linklist.add("0");
+        assertThat(linklist.remove(), is("0"));
+        linklist.get(0);
+    }
+
+    /**
      * Тест методов листа.
      * Добавляем 2 элементов.
-     * Убеждаемся, что remove удаляет нужный элемент.
      * Убеждаемся, что get корректно работает в обе стороны.
+     * Убеждаемся, что remove() удаляет нужный элемент.
+     * Убеждаемся, что remove(index) удаляет нужный элемент.
      */
+
     @Test (expected = IndexOutOfBoundsException.class)
     public void addDeleteGetTest() {
         SimpleLinkedList<String> linklist = new SimpleLinkedList<>();
         linklist.add("0");
         linklist.add("1");
-        linklist.add("2");
         assertThat(linklist.get(0), is("0"));
-        assertThat(linklist.get(2), is("2"));
-        assertThat(linklist.remove(1), is("1"));
+        assertThat(linklist.get(1), is("1"));
         assertThat(linklist.remove(), is("0"));
-        linklist.get(1);
+        assertThat(linklist.remove(0), is("1"));
+        linklist.get(0);
     }
+
     /**
      * Тест итератора LinkedList.
+     * Убеждаемся, что hasNext() работает и не двигает каретку.
+     * Убеждаемся, что next() работает и двигает каретку.
      */
     @Test (expected = NoSuchElementException.class)
     public void iteratorOfSimpleArrayListTest() {
         SimpleLinkedList<String> linklist = new SimpleLinkedList<>();
         Iterator<String> iterator = linklist.iterator();
-        for (int i = 0; i < 5; i++) {
-            linklist.add(String.valueOf(i));
-        }
+        linklist.add("0");
         assertThat(iterator.hasNext(), is(true));
-        iterator.hasNext();
         assertThat(iterator.next(), is("0"));
-        assertThat(iterator.next(), is("1"));
-        assertThat(iterator.next(), is("2"));
-        iterator.remove();
-        assertThat(linklist.get(2), is("3"));
-        assertThat(iterator.next(), is("3"));
-        assertThat(iterator.hasNext(), is(true));
-        assertThat(iterator.next(), is("4"));
         assertThat(iterator.hasNext(), is(false));
         iterator.next();
+
     }
 
 }
