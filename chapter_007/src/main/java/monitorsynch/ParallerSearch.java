ff79554673b3f1329eb029e38ea39c33954d0ab2
HeTyDeHer <dolboeb88@gmail.com>
2017-12-06 18:08:47 +0300

    4. Поиск текста в файле.[#32869]

--- chapter_007/src/main/java/monitorsynch/ParallerSearch.java
+++ chapter_007/src/main/java/monitorsynch/ParallerSearch.java
@@ -6,7 +6,9 @@ import net.jcip.annotations.ThreadSafe;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * 4. Поиск текста в файле. [#1106].
@@ -23,9 +25,6 @@ public class ParallerSearch {
     /** Пути к файлам, удовлетворяющим условиям поиска. */
     @GuardedBy("this")
     private final List<String> result = new ArrayList<>();
-    /** Запущенные треды. */
-    @GuardedBy("threads")
-    private final ArrayList<Thread> threads = new ArrayList<>();
 
     /**
      * Конструктор.
@@ -43,7 +42,7 @@ public class ParallerSearch {
      * Геттер.
      * @return Массив с путями к файлам, удовлетворяющим условиям поиска.
      */
-    public List<String> getResult() {
+    private List<String> getResult() {
         return result;
     }
 
@@ -52,121 +51,49 @@ public class ParallerSearch {
      * @param result Путь к файлу, удовлетворяющим условиям поиска.
      */
     @GuardedBy("this")
-    public synchronized void addResult(String result) {
+    private synchronized void addResult(String result) {
         this.result.add(result);
     }
 
     /**
      * Начало парсинга. Проверяем правильность введенного пути и запускаем поток парсинга директории.
+     * Получаем List с найденными файлами нужного расширения, запускаем их парсинг отдельными потоками.
      * @throws InterruptedException InterruptedException
      */
-    @GuardedBy("threads")
-    public void beginParsing() throws InterruptedException {
+
+    public void parsing() throws InterruptedException {
         File dir = new File(this.root);
         if (!dir.isDirectory()) {
             System.out.println("Wrong root");
             return;
-        } else {
-            Thread thread = new Thread(new ParseDir(dir));
-            synchronized (threads) {
-                threads.add(thread);
+        }
+
+        FutureTask<List<File>> task = new FutureTask<>(new DirParser(dir));
+        new Thread(task).start();
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        try {
+            for (File file : task.get()) {
+                executor.submit(new ParseFile(file));
             }
-            thread.start();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        executor.shutdown();
+        executor.awaitTermination(100000000, TimeUnit.SECONDS); // todo разобраться с shutdown
+
+        for (String s: this.getResult()) {
+            System.out.println(s);
         }
     }
 
-    /**
-     * Мне не нравится этот метод, но ничего умнее я не придумал.
-     * Проверяем, все ли треды завершились.
-     * В массиве запущенных тредов threads проверяем, есть ли живые треды. Если обнаруживаем модификацию массива
-     * во время работы метода (ConcurentModification), то перезапускаем метод.
-     * @throws InterruptedException InterruptedException
-     */
-    private void checkIfAllThreadsAreFinished() throws InterruptedException {
-        boolean finished = false;
-        while (!finished) {
-            try {
-                for (Thread t : this.threads) {
-                    if(t.isAlive()) {
-                        finished = false;
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        break;
-                    } else {
-                        finished = true;
-                    }
-                }
-            } catch (Exception e) {
-                Thread.sleep(1000);
-                checkIfAllThreadsAreFinished();
-            }
-        }
-    }
-
-    /**
-     * Класс, который парсит директории на наличие файлов искомого расширения.
-     * Если таковой находится, запускает парсинг файла отдельным потоком.
-     */
-    private class ParseDir implements Runnable {
-        /** Директория для парсинга. */
-        private File dir;
-
-        /**
-         * Конструктор.
-         * @param dir Директория для парсинга.
-         */
-        private ParseDir(File dir) {
-            this.dir = dir;
-        }
-
-        /**
-         * Добавление созданного треда в массив тредов.
-         * @param t созданный тред.
-         */
-        private void addThread(Thread t) {
-            synchronized (threads) {
-                threads.add(t);
-            }
-        }
-
-        /**
-         * Если обнаруживаем папку, запускаем её парсинг отдельным тредом.
-         * Если обнаруживаем файл нужного расширения, запускаем его парсинг отдельным тредом.
-         * Все созданные треды добавляем в массив тредов.
-         */
-        @Override
-        public void run() {
-            File [] files = dir.listFiles();
-            if (files == null) {
-                return;
-            }
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    Thread t = new Thread(new ParseDir(file));
-                    addThread(t);
-                    t.start();
-                } else if (file.isFile()){
-                    for (String ext : extentions) {
-                        if (file.getName().endsWith(ext)) {
-                            Thread t = new Thread(new ParseFile(file));
-                            addThread(t);
-                            t.start();
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
 
     /**
      * Парсинг файла. Проверяем наличие искомого текста.
      */
     private class ParseFile implements Runnable {
         /** Файл. */
+
         private File file;
 
         /**
@@ -199,24 +126,62 @@ public class ParallerSearch {
         }
     }
 
+    /**
+     * Парсинг папки.
+     * Если находим вложенную папку - запускаем отдельный поток.
+     * Возвращаем List с файлами.
+     */
+    public class DirParser implements Callable<List<File>> {
+         /** Папка для поиска. */
+        private File dir;
+        /** Лист с найденными файлами. */
+        private List<File> resultList;
+
+        public DirParser(File dir) {
+            this.dir = dir;
+            this.resultList = new ArrayList<>();
+        }
+
+        @Override
+        public List<File> call() throws Exception {
+            File [] files = dir.listFiles();
+            if (files == null) {
+                return resultList;
+            }
+
+            ArrayList<Future> results = new ArrayList<>();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    FutureTask<List<File>> task = new FutureTask<>(new DirParser(file));
+                    results.add(task);
+                    Thread t = new Thread(task);
+                    t.start();
+                } else if (file.isFile()){
+                    for (String ext : extentions) {
+                        if (file.getName().endsWith(ext)) {
+                            resultList.add(file);
+                        }
+                    }
+                }
+            }
+            for (Future f : results) {
+                resultList.addAll((Collection<? extends File>) f.get());
+            }
+            return resultList;
+        }
+    }
 
     public static void main(String[] args) {
         long time = System.currentTimeMillis();
         ParallerSearch ps = new ParallerSearch("C:/", "test", new ArrayList<>(Arrays.asList("txt")));
         try {
-            ps.beginParsing();
-            ps.checkIfAllThreadsAreFinished();
+            ps.parsing();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println((System.currentTimeMillis() - time)/1000);
-        System.out.println(ps.getResult().size());
 
-        for (String s: ps.getResult()) {
-            System.out.println(s);
-        }
     }
-
 }
 
 
