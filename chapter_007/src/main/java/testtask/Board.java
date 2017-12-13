6db13228c463dc0e8908fdbe1e269546d197e1f1
HeTyDeHer <dolboeb88@gmail.com>
2017-12-13 13:02:05 +0300

    Тестовое задание. [#1108]fix

--- chapter_007/src/main/java/testtask/Board.java
+++ chapter_007/src/main/java/testtask/Board.java
@@ -60,7 +60,7 @@ class Hero implements Runnable {
             xPos = nextX;
             yPos = nextY;
             try {
-                Thread.sleep(100);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
